885e0b7702cd7148f59ba5c30c8060ba5cb3060e
Ramazan Yılmaz <ramazanyilmaz1771@hotmail.com>
2021-02-28 02:13:35 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri m)
+        {
+            Console.WriteLine(m.musteriAd + " " + m.musteriSoyad + " " + m.musteriYas);
+        }
+
+        public void Listeleme(Musteri m)
+        {
+
+            Console.WriteLine(m.musteriAd + " " + m.musteriSoyad + " " + m.musteriYas);
+        }
+
+        public void Silme(Musteri m)
+        {
+            Console.WriteLine(m.musteriAd + " " + m.musteriSoyad + " " + m.musteriYas);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri();
+            m1.musteriAd = "Ali";
+            m1.musteriSoyad = "Yılmaz";
+            m1.musteriYas = 20;
+
+            Musteri m2 = new Musteri();
+            m2.musteriAd = "Ahmet";
+            m2.musteriSoyad = "Bayram";
+            m2.musteriYas = 27;
+
+            Musteri m3 = new Musteri();
+            m3.musteriAd = "Oğuz";
+            m3.musteriSoyad = "Sert";
+            m3.musteriYas = 30;
+
+            Musteri[] musteriler = new Musteri[] { m1, m2, m3 };
+            Console.WriteLine("------------------Müşterilerimiz----------------");
+            foreach (Musteri m in musteriler)
+            {
+                Console.WriteLine(m.musteriAd + " " + m.musteriSoyad + " " + m.musteriYas);
+            }
+
+            Console.WriteLine("-------------Müşteri Ekleniyor-------------\n");
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(m1);
+
+            Console.WriteLine("-------------Müşteriler Listeleniyor-----------\n");
+            musteriManager.Listeleme(m1);
+            musteriManager.Listeleme(m2);
+            musteriManager.Listeleme(m3);
+
+            Console.WriteLine("-------------Müşteri Siliniyor-----------\n");
+            musteriManager.Silme(m3);
+        }
+    }
+}
